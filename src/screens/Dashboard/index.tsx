28facaa3dafab58903d5e15d584868849bca0005
Andre Lino <andreclino@gmail.com>
2021-07-18 18:27:10 -0300

    salvando

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
-import {Container, Title} from './styles';
+import {    
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGretting,
+    UserName,
+    UserWrapper,
+    Icon
+} from './styles';
 
 export function Dashboard() {
 
     return (
-        <Container>
-            <Title>Dashboard</Title>
+        <Container>            
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={
+                            {uri: 'https://avatars.githubusercontent.com/u/4925220?v=4'}
+                            }/>
+                        <User>
+                            <UserGretting>Olá, </UserGretting>
+                            <UserName>Andre</UserName>
+                        </User>
+                    </UserInfo>
+                    
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>            
         </Container>
     );
 
